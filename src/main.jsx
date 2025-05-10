4a04aef347deae4359fd6a8081fbc49169fbb52a
Jenaru Matei <mateijeanru123@gmail.com>
2025-05-10 13:46:50 +0300

    Add m

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
+  </StrictMode>,
 )
+
+setTimeout(() => {
+  const root = document.getElementById("root");
+  root.style.display = "none";
+  root.offsetHeight; // Force reflow
+  root.style.display = "";
+}, 100);
